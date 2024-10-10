ac163ff079d479d76a2cef58efc5c88faa233eae
uzzt <uzzzt@outlook.com>
2024-10-10 17:50:06 +0800

    feat: pnpm workspace

---
+++ packages/a/index.mjs
@@ -0,0 +1,9 @@
+import {b} from '@uzzt/b';
+import { utils } from '@uzzt/utils';
+
+console.log('a - init');
+
+console.log(b());
+
+console.log(utils());
+export const a = () => { console.log('a-package') }
